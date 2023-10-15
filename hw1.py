bbecebb7878a1349180b5f37d0f52e6c8a6feaf8
Teslim Olayiwola <olayiwolateslim9@gmail.com>
2023-10-15 12:37:08 -0500

    revise the submission hw1.py

--- hw1.py
+++ hw1.py
@@ -32,6 +32,12 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 
 ## Training data
+## Training data
+def convert_labels(labels):
+    converted = torch.zeros(labels.size(0), 2)
+    converted[labels.view(-1) == 1, 0] = 1
+    converted[labels.view(-1) == 0, 1] = 1
+    return converted
 class MidiDataProcessor:
 
     def __init__(self, data_directory, maxlen=100, test_size=0.2, random_state=42, batch_size=32):
@@ -44,7 +50,7 @@ class MidiDataProcessor:
     def __get__(self, idx):
         return self.all_data[idx], self.all_labels[idx]
 
-    def prepare_data(self, is_scale=False):
+    def prepare_data(self):
         device = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
         all_midis = glob.glob(f'{self.data_directory}/maestro-v1.0.0/**/*.midi')
 
@@ -66,67 +72,93 @@ class MidiDataProcessor:
                                                                                     test_size=self.test_size,
                                                                                     random_state=self.random_state,
                                                                                     shuffle=True)
-        features_train = features_train.reshape((-1, features_train.shape[1], 1))
-        features_test = features_test.reshape((-1, features_test.shape[1], 1))
-
-        label_train = label_train.reshape((-1, 1))
-        label_test = label_test.reshape((-1, 1))
-
-        def convert_labels(labels):
-            converted = torch.zeros(labels.size(0), 2)
-            converted[labels.view(-1) == 1, 0] = 1
-            converted[labels.view(-1) == 0, 1] = 1
-            return converted
         
-        label_train = convert_labels(label_train)
-        label_test = convert_labels(label_test)
-        
-        if is_scale:
-            scaler = MinMaxScaler()
-            features_train = scaler.fit_transform(features_train)
-            features_test = scaler.transform(features_test)
+        # label_train = convert_labels(label_train)
+        # label_test = convert_labels(label_test)
 
         features_train = torch.Tensor(features_train).to(device)
         features_test = torch.Tensor(features_test).to(device)
+
         label_train = torch.Tensor(label_train).to(device)
         label_test = torch.Tensor(label_test).to(device)
 
         train_dataset = TensorDataset(features_train, label_train)
         test_dataset = TensorDataset(features_test, label_test)
 
-        train_loader = DataLoader(train_dataset, shuffle=True, batch_size=self.batch_size)
-        test_loader = DataLoader(test_dataset, shuffle=True, batch_size=self.batch_size)
+        self.train_loader = DataLoader(train_dataset, shuffle=True, batch_size=self.batch_size)
+        self.test_loader = DataLoader(test_dataset, shuffle=True, batch_size=self.batch_size)
 
-        return train_loader, test_loader
+        return self.train_loader, self.test_loader
 
     def __repr__(self):
-        return f'MidiDataProcessor(data_directory={self.data_directory!r}, maxlen={self.maxlen}, test_size={self.test_size}, random_state={self.random_state}, batch_size={self.batch_size})'
+        return f'MidiDataProcessor(data_directory={self.data_directory!r}, maxlen={self.maxlen}, test_size={self.test_size}, random_state={self.random_state}, batch_size={self.batch_size}, train_loader size={len(self.train_loader.dataset)}, test_loader size={len(self.test_loader.dataset)})'
 
 # Critic model
+# n x max_len -> embedding -> n x nax_len x max_len - 1 -> LSTM (with hidden size=3)-> n x 2
 class LSTMCritic(nn.Module):
-    def __init__(self, input_dim=1, hidden_size=64, num_layers=3, n_classes=2):
+    def __init__(self, num_embeddings=382, embedding_dim=100, hidden_dim=128, num_layers=3, n_classes=2):
         super(LSTMCritic, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.output_size = n_classes
+        self.num_embeddings = num_embeddings # number of unique words in the vocabulary
+        self.embedding_dim = embedding_dim #
+        self.hidden_dim = hidden_dim # Hidden dimension
+        self.num_layers = num_layers # Number of LSTM layers
         self.device = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
-        self.lstm = nn.LSTM(input_size = self.input_dim, hidden_size = self.hidden_size, num_layers = self.num_layers, batch_first=True)
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
+        
+        self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim).to(self.device)
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_dim, num_layers=self.num_layers, batch_first=True, dropout=0.2).to(self.device)
+        self.fc = nn.Linear(self.hidden_dim, n_classes).to(self.device)
+        
 
     def forward(self, x):
-        batch_size = x.size(0)
-        # Initialize hidden state with zeros
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(self.device)
+        # Embedding layer
+        x = self.embedding(x).to(self.device)
         # LSTM forward pass
-        out, _ = self.lstm(x, (h0, c0))
+        batch_size = x.size(0)
+        # Initialize hidden and cell states with zeros
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(self.device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_dim).to(self.device)
+        lstm_out, _ = self.lstm(x, (h0, c0))
         # Decode the hidden state of the last time step
-        out = out[:, -1, :]
-        # linear layer
+        out = lstm_out[:, -1, :]
+        # Linear layer
         out = self.fc(out)
         return out
 
+# Accumulation meter
+class AccumulationMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = 0.0
+        self.avg = 0.0
+        self.sum = 0
+        self.count = 0.0
+
+    def update(self, value, n=1):
+        self.value = value
+        self.sum += value * n
+        self.count += n
+        self.avg = self.sum / self.count
+        self.sqrt = self.value ** 0.5
+        self.rmse = self.avg ** 0.5
+
+# Early stoppping criteria
+class EarlyStopping:
+    def __init__(self, tolerance=5, min_delta=0):
+
+        self.tolerance = tolerance
+        self.min_delta = min_delta
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, train_loss, validation_loss):
+        if (validation_loss - train_loss) > self.min_delta:
+            self.counter +=1
+            if self.counter >= self.tolerance:  
+                self.early_stop = True
 # Critic class
 class Critic(CriticBase):
     def __init__(self, load_trained=False):
@@ -139,7 +171,7 @@ class Critic(CriticBase):
     
         self.load_trained = load_trained
         self.device = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
-        self.model = LSTMCritic(input_dim=1, hidden_size=64, num_layers=3, n_classes=2).to(self.device)
+        self.model = LSTMCritic().to(self.device)
         self.criterion = nn.CrossEntropyLoss()
         if self.load_trained:
             logging.info('load model from file ...')
@@ -163,6 +195,25 @@ class Critic(CriticBase):
             outputs ^= 1 # index 0 is good and index 1 is bad 
                     
         return outputs  
+    
+    def validate(self, val_loader, model):
+        """Evaluate the network on the entire validation set."""
+
+        loss_accum = AccumulationMeter()
+        model.eval()
+        with torch.set_grad_enabled(False):
+
+            for i, (feature, label) in enumerate(val_loader):
+                label = convert_labels(label) # convert labels to one-hot encoding
+                feature, label = feature.to(self.device).long(), label.to(self.device)
+
+                outputs = self.model(feature)
+
+                loss = self.criterion(outputs, label)
+                loss_accum.update(loss.item(), label.size(0))
+ 
+
+        return loss_accum.rmse
 
     def train(self, x, epochs=10, lr=1e-5):
         '''
@@ -172,28 +223,66 @@ class Critic(CriticBase):
         '''
             
         optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        loss_accum_train = AccumulationMeter()
 
-        logging.info('Start training ...')
-        self.model.train()
-        total_loss = 0
-        for epoch in range(epochs):
-            for idx, (feature, label) in enumerate(x):
-                feature, label = feature.to(self.device), label.to(self.device)
+        # split data for K-fold cross validation to avoid overfitting
+        indices = list(range(len(x.dataset)))
+        kf = KFold(n_splits=5, shuffle=True)
+        cv_index = 0
+        index_list_train = []
+        index_list_valid = []
+        for train_indices, valid_indices in kf.split(indices):
+            index_list_train.append(train_indices)
+            index_list_valid.append(valid_indices)
 
-                optimizer.zero_grad()
-                outputs = self.model(feature)
-                loss = self.criterion(outputs, label)
-                total_loss += loss.item()
-                loss.backward()
-                optimizer.step()
+            train_sampler = SubsetRandomSampler(train_indices)
+            valid_sampler = SubsetRandomSampler(valid_indices)
 
-            print(f"Epoch {epoch+1}/{epochs}, Loss: {loss.item()}")
+            train_loader = DataLoader(x.dataset, batch_size=32,
+                                                       sampler=train_sampler,
+                                                       shuffle=False)
+            val_loader = DataLoader(x.dataset, batch_size=32,
+                                                     sampler=valid_sampler,
+                                                     shuffle=False)
+
+            logging.info('Start training ...')
+            self.model.train()
+            early_stopping = EarlyStopping(tolerance=5, min_delta=10)
+            epoch_train_loss = []
+            epoch_validate_loss = []
+            for epoch in range(epochs):
+                for idx, (feature, label) in enumerate(train_loader):
+                    label = convert_labels(label) # convert labels to one-hot encoding
+                    feature, label = feature.to(self.device).long(), label.to(self.device)
+
+                    outputs = self.model(feature)
+                    loss = self.criterion(outputs, label)
+                    #total_loss += loss.item()
+                    loss_accum_train.update(loss.item(), label.size(0))
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                    torch.backends.cudnn.enabled = False
+                # training loss
+                epoch_train_loss.append(loss_accum_train.avg)
+                
+                # validation loss
+                val_loss_avg = self.validate(val_loader, self.model)
+                epoch_validate_loss.append(val_loss_avg)
+
+                print(f"Epoch {epoch+1}/{epochs}, Train Loss: {loss_accum_train.avg}, Val Loss: {val_loss_avg}")
+
+                # early stopping
+                early_stopping(loss_accum_train.avg, val_loss_avg)
+                if early_stopping.early_stop:
+                    print("We are at epoch:", epoch)
+                    logging.info("Finished training ...Model saved")
+                    torch.save(self.model, 'critic.pth') 
+                    break
         
-        logging.info("Finished training ...")
-
-        torch.save(self.model, 'critic.pth')
+            cv_index += 1   # increment cv index
         
-        return total_loss/feature.size(0)
+        return loss_accum_train.avg
 
 ##*************************Task 2*********************#
 #   (Class "Composer" should be a subclass of the class ComposerBase. You must use the exact class name.) 
